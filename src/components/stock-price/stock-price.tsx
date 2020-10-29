096f3f973d35beb056845d685fb11f658e5b0068
Emilio <malioml@gmail.com>
2020-10-29 20:56:41 +0100

    adding watcher to update price onChange input and prop

--- src/components/stock-price/stock-price.tsx
+++ src/components/stock-price/stock-price.tsx
@@ -1,4 +1,4 @@
-import { Component, h, State, Element } from '@stencil/core';
+import { Component, h, State, Element, Prop, Watch } from '@stencil/core';
 import { AV_API_KEY } from '../../global/global';
 
 @Component({
@@ -8,11 +8,22 @@ import { AV_API_KEY } from '../../global/global';
 })
 export class StockPrice {
   stockInput: HTMLInputElement;
+  initialStockSymbol: string;
   @Element() el: HTMLElement;
   @State() fetchedPrice: number;
   @State() stockUserInput: string;
   @State() stockInputValid = false;
   @State() error: string;
+
+  @Prop({ mutable: true, reflect:true }) stockSymbol: string;
+
+  @Watch('stockSymbol')
+  stockSymbolChanged(newValue: string, oldValue: string) {
+    if (newValue !== oldValue) {
+      this.stockUserInput = newValue;
+      this.fetchStockPrice(newValue);
+    }
+  }
 
   onUserInput(event: Event) {
     this.stockUserInput = (event.target as HTMLInputElement).value;
@@ -26,18 +37,48 @@ export class StockPrice {
   onFetchStockPrice(event) {
     event.preventDefault();
     // const stockSymbol = (this.el.shadowRoot.querySelector('#stock-symbol') as HTMLInputElement).value
-    const stockSymbol = this.stockInput.value;
+    this.stockSymbol = this.stockInput.value;
+    // this.fetchStockPrice(stockSymbol);
+  }
+
+  componentDidLoad() {
+    if (this.stockSymbol) {
+      this.initialStockSymbol = this.stockSymbol;
+      this.stockUserInput = this.stockSymbol;
+      this.stockInputValid = true;
+      this.fetchStockPrice(this.stockSymbol);
+    }
+  }
+
+  componentWillLoad() {
+    console.log('componentWillLoad');
+    console.log(this.stockSymbol);
+  }
+
+  componentWillUpdate() {
+    console.log('componentWillUpdate');
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate');
+    if (this.stockSymbol !== this.initialStockSymbol) {
+      this.initialStockSymbol = this.stockSymbol;
+      this.fetchStockPrice(this.stockSymbol);
+    }
+  }
+
+  fetchStockPrice(stockSymbol: string) {
     fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stockSymbol}&apikey=${AV_API_KEY}`)
       .then(res => {
         return res.json();
       }).then(parsedRes => {
-        if (!parsedRes || !parsedRes['Global Quote'] || !parsedRes['Global Quote']['05. price']) {
-          this.fetchedPrice = null;
-          throw new Error('Invalid symbol!');
-        }
-        this.error = null;
-        this.fetchedPrice = +parsedRes['Global Quote']['05. price'];
-      })
+      if (!parsedRes || !parsedRes['Global Quote'] || !parsedRes['Global Quote']['05. price']) {
+        this.fetchedPrice = null;
+        throw new Error('Invalid symbol!');
+      }
+      this.error = null;
+      this.fetchedPrice = +parsedRes['Global Quote']['05. price'];
+    })
       .catch(err => this.error = err.message);
   }
 
