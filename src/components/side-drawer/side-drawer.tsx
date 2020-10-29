c043d299e5d03735d6e955f2835ba540f5b7a4c5
Emilio <malioml@gmail.com>
2020-10-29 14:52:52 +0100

    created side-drawer component in stencil

---
+++ src/components/side-drawer/side-drawer.tsx
@@ -0,0 +1,59 @@
+import { Component, h, Prop, State, Method } from '@stencil/core';
+
+@Component({
+  tag: 'uc-side-drawer',
+  styleUrl: './side-drawer.css',
+  shadow: true,
+})
+export class SideDrawer {
+  //reflect: true -> changes in attributes also change DOM.
+  @Prop({reflect: true}) title: string;
+  @Prop({reflect: true, mutable: true}) opened: boolean;
+  @State() showContactInfo = false;
+
+  onCloseDrawer(event) {
+    event.preventDefault();
+    this.opened = false;
+  }
+
+  onContentChange(content: string) {
+    this.showContactInfo = content === 'contact';
+  }
+
+  @Method()
+  open() {
+    this.opened = true;
+  }
+
+  render() {
+    let mainContent = <slot />;
+    if (this.showContactInfo) {
+      mainContent = (
+        <div id="contact-information">
+          <h2>Contact Information</h2>
+          <p>You can reach us via phone or email.</p>
+          <ul>
+            <li>Phone: 49802384032</li>
+            <li>Email: <a href="mailto: something@something.com"></a></li>
+          </ul>
+        </div>
+      );
+    }
+    return [
+      <div class="backdrop" onClick={this.onCloseDrawer.bind(this)}></div>,
+      <aside>
+        <header>
+          <h1>{this.title}</h1>
+          <button onClick={this.onCloseDrawer.bind(this)}>X</button>
+        </header>
+        <section id="tabs">
+          <button class={!this.showContactInfo ? 'active' : ''} onClick={this.onContentChange.bind(this, 'nav')}>Navigation</button>
+          <button class={this.showContactInfo ? 'active' : ''} onClick={this.onContentChange.bind(this, 'contact')}>Contact</button>
+        </section>
+        <main>
+          {mainContent}
+        </main>
+      </aside>
+    ];
+  }
+}
